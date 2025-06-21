76c1fa8a87148a5ad9871202045067608edcace7
MattyP2005 <matias1976.perez@gmail.com>
2025-06-21 14:42:05 -0500

    Corrijo .gitignore para proyecto

--- Flotas.MVC/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace Flotas.MVC.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
